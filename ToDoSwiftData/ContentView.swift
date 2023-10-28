8a704273227421d8089d74f7657f0d9102eb1006
rotem <nevgauker@gmail.com>
2023-10-28 18:50:55 +0300

    Initial Commit

---
+++ ToDoSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoSwiftData
+//
+//  Created by Rotem Nevgauker on 28/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoSwiftData/ToDoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataApp.swift
+//  ToDoSwiftData
+//
+//  Created by Rotem Nevgauker on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
